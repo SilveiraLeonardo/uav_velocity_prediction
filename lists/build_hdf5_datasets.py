1517470cafa6afc99e0ede0856e45780ed86ff75
SilveiraLeonardo <47951669+SilveiraLeonardo@users.noreply.github.com>
2022-06-15 08:01:34 -0300

    Add files via upload

--- lists/build_hdf5_datasets.py
+++ lists/build_hdf5_datasets.py
@@ -1,4 +1,5 @@
 from model.hdf5datasetwriter import HDF5DatasetWriter
+from model.aspectawarepreprocessor import AspectAwarePreprocessor
 import cv2
 import imutils
 import pandas as pd
@@ -7,7 +8,12 @@ import numpy as np
 def preprocess(image):
     # resize to 128x128x3
     # to grayscale
-    
+    aap = AspectAwarePreprocessor(128, 128)
+    image = aap.preprocess(image)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = np.expand_dims(image, axis=-1)
+    return image
+
 TRAIN_HDF5 = "datasets/hdf5/train.hdf5"
 VALID_HDF5 = "datasets/hdf5/val.hdf5"
 TEST_HDF5 = "datasets/hdf5/test.hdf5"
@@ -34,9 +40,14 @@ datasets = [
 
 for (paths1, paths2, labels, outputPath) in datasets:
     print("[INFO] building {}...".format(outputPath))
-    writer = HDF5DatasetWriter((len(paths), 128, 128, 1), outputPath)
+    writer = HDF5DatasetWriter((len(paths1), 128, 128, 1), outputPath)
 
     for (path1, path2, label) in zip(paths1, paths2, labels):
-        image = cv2.imread(path1)
-        image = preprocess(image)
+        image1 = cv2.imread(path1)
+        image1 = preprocess(image1)
+
+        image2 = cv2.imread(path2)
+        image2 = preprocess(image2)    
+
+        writer.add([image1], [image2], [label])
 
