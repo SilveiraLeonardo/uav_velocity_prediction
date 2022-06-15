c5b9f87c1983db4ec35a195b12c8c976776baf6e
SilveiraLeonardo <47951669+SilveiraLeonardo@users.noreply.github.com>
2022-06-15 09:45:27 -0300

    Add files via upload

--- train_network.py
+++ train_network.py
@@ -1,6 +1,12 @@
 # import the necessary packages
-from model.model import build_siamese_model
-from tensorflow.keras.models import Model
+import matplotlib
+matplotlib.use("Agg")
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from model.siamese_network import build_siamese_model
+from model.trainingMonitor import TrainingMonitor
+from model.hdf5datasetgenerator import HDF5DatasetGenerator
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Conv2D
@@ -11,25 +17,31 @@ from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import concatenate
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.callbacks import ModelCheckpoint
+import os
 
-def monitor_training(H):
-	plt.style.use("ggplot")
-	plt.figure()
-	plt.plot(np.arange(0, len(H.history["loss"])), H.history["loss"], label="train_loss")
-	plt.plot(np.arange(0, len(H.history["loss"])), H.history["val_loss"], label="val_loss")
-	plt.plot(np.arange(0, len(H.history["loss"])), H.history["accuracy"], label="train_acc")
-	plt.plot(np.arange(0, len(H.history["loss"])), H.history["val_accuracy"], label="val_accuracy")
-	plt.title("Training Loss and Accuracy")
-	plt.xlabel("Epoch #")
-	plt.ylabel("Loss/Accuracy")
-	plt.legend()
-	plt.savefig("plots/monitor_{}.png".format(len(H.history["loss"])))
+# def monitor_training(H):
+# 	plt.style.use("ggplot")
+# 	plt.figure()
+# 	plt.plot(np.arange(0, len(H.history["loss"])), H.history["loss"], label="train_loss")
+# 	plt.plot(np.arange(0, len(H.history["loss"])), H.history["val_loss"], label="val_loss")
+# 	plt.plot(np.arange(0, len(H.history["loss"])), H.history["accuracy"], label="train_acc")
+# 	plt.plot(np.arange(0, len(H.history["loss"])), H.history["val_accuracy"], label="val_accuracy")
+# 	plt.title("Training Loss and Accuracy")
+# 	plt.xlabel("Epoch #")
+# 	plt.ylabel("Loss/Accuracy")
+# 	plt.legend()
+# 	plt.savefig("plots/monitor_{}.png".format(len(H.history["loss"])))
 
-IMG_SHAPE = (240, 240, 3)
+IMG_SHAPE = (128, 128, 1)
 chanDim = -1
 BATCH_SIZE = 64
 EPOCHS = 100
-MODEL_PATH = "model/velocity_pred_model"
+MODEL_PATH = "checkpoints/velocity_pred_model"
+FIG_PATH = "plots/monitor_{}.png".format(os.getpid())
+TRAIN_HDF5 = "datasets/hdf5/train.hdf5"
+VALID_HDF5 = "datasets/hdf5/val.hdf5"
+TEST_HDF5 = "datasets/hdf5/test.hdf5"
 
 # configure the siamese network
 print("[INFO] build network...")
@@ -40,51 +52,60 @@ featsA = featureExtractor(imgA)
 featsB = featureExtractor(imgB)
 
 x = concatenate([featsA, featsB], axis=chanDim)
-x = Conv2D(512, (3,3), padding="same", activation="relu")(x)
+x = Conv2D(128, (3,3), padding="same", activation="relu")(x)
 x = BatchNormalization(axis=chanDim)(x)
-x = Conv2D(512, (3,3), padding="same", activation="relu")(x)
+x = Conv2D(128, (3,3), padding="same", activation="relu")(x)
 x = BatchNormalization(axis=chanDim)(x)
 x = MaxPooling2D(pool_size=2)(x)
 x = Dropout(0.2)(x)
 
-x = Conv2D(512, (3,3), padding="same", activation="relu")(x)
+x = Conv2D(256, (3,3), padding="same", activation="relu")(x)
 x = BatchNormalization(axis=chanDim)(x)
-x = Conv2D(512, (3,3), padding="same", activation="relu")(x)
+x = Conv2D(256, (3,3), padding="same", activation="relu")(x)
 x = BatchNormalization(axis=chanDim)(x)
 x = MaxPooling2D(pool_size=2)(x)
 x = Dropout(0.2)(x)
 
 x = Flatten()(x)
-x = Dense(512, activation="relu")(x)
+x = Dense(256, activation="relu")(x)
 x = BatchNormalization()(x)
 x = Dropout(0.2)(x)
-x = Dense(512, activation="relu")(x)
+x = Dense(256, activation="relu")(x)
 x = BatchNormalization()(x)
 output = Dense(1, activation="linear")(x)
 
 model = Model(inputs=[imgA, imgB], outputs=output)
 
+trainGen = HDF5DatasetGenerator(TRAIN_HDF5, BATCH_SIZE)
+valGen = HDF5DatasetGenerator(VALID_HDF5, BATCH_SIZE)
+
 # best model checkpoint
 ckp_path = "checkpoints/model_{epoch:02d}.hdf5"
-mcp = tf.keras.callbacks.ModelCheckpoint(filepath=ckp_path,
-										monitor="val_loss",
-										save_best_only=True,
-										mode="auto",
-										save_freq="epoch",
-										verbose=1)
+mcp = ModelCheckpoint(filepath=ckp_path,
+					monitor="val_loss",
+					save_best_only=True,
+					mode="auto",
+					save_freq="epoch",
+					verbose=1)
+
+callbacks=[mcp, TrainingMonitor(FIG_PATH)]
 
 print("[INFO] compiling model...")
 model.compile(optimizer="adam", loss=MeanSquaredError(), metrics=["accuracy"])
 
 print("[INFO] training model...")
-history = model.fit(
-	[pairTrain[:,0], pairTrain[:,1]], labelTrain[:],
-	validation_data=([pairTest[:,0], pairTest[:,1]], labelTest[:]),
-	batch_size = BATCH_SIZE,
-	epochs= EPOCHS)
+model.fit(
+	trainGen.generator(),
+	steps_per_epoch = trainGen.numImages // BATCH_SIZE,
+	validation_data=valGen.generator(),
+	validation_steps = valGen.numImages // BATCH_SIZE,
+	epochs = EPOCHS,
+	max_queue_size = 10,
+	callbacks = callbacks,
+	verbose = 1)
 
 print("[INFO] serializing model")
-model.save(MODEL_PATH)
+model.save(MODEL_PATH, overwrite=True)
 
-print("[INFO] plotting training history")
-monitor_training(history)
+trainGen.close()
+valGen.close()
